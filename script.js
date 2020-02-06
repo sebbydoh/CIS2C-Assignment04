26680a4383bc13df417ed296e2fa1f68a8207aa6
Encinia <sebastienencinia@my.whccd.edu>
2020-02-05 18:49:34 -0800

    Completed Question 2, a function that returns the circumference of a circle when given the diameter

--- script.js
+++ script.js
@@ -3,9 +3,18 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
+
+
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
+
+function circumference(diameter) {
+    const pi = 3.14;
+    return pi * diameter;
+}
+
+console.log(circumference(10));
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
